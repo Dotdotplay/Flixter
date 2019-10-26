a46183a8a83de03caa659450037597a2e45c9963
Muhammad Raqib <muhammad.raqib1@gmail.com>
2019-10-26 00:31:50 -0400

    Added Youtube Video trailer Support for each movie

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,7 +59,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -64,27 +70,40 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.idPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
-            tvTitle.setText(movie.getTitle());
-            tvOverview.setText(movie.getOverview());
-            String imageUrl;
+            public void bind(final Movie movie) {
+                tvTitle.setText(movie.getTitle());
+                tvOverview.setText(movie.getOverview());
+                String imageUrl;
 
-            // if phone is in landscape
-            // then image url == backdrop
-            // else poster image
+                // if phone is in landscape
+                // then image url == backdrop
+                // else poster image
 
-            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                imageUrl = movie.getBackdropPath();
-            } else {
-                imageUrl = movie.getPosterPath();
+                if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    imageUrl = movie.getBackdropPath();
+                } else {
+                    imageUrl = movie.getPosterPath();
+                }
+
+
+                Glide.with(context).load(imageUrl).into(ivPoster);
+
+                //Register click listener
+                // navigate to new detail view
+                container.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(context, DetailActivity.class);
+
+                        i.putExtra("movie", Parcels.wrap(movie));
+                        context.startActivity(i);
+                    }
+                });
+
             }
-
-
-            Glide.with(context).load(imageUrl).into(ivPoster);
-
-
-        }
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,23 +3,34 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Parcel
 public class Movie {
+    int movieId;
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    double rating;
 
+    // EMpty constructor for the parceler library
+    public Movie () {
+
+    }
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
+
+
 
     public static List<Movie> fromJsonArray (JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
@@ -42,5 +53,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
